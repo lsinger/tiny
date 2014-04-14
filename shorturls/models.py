55b54b98ae6564181e34945de33ec95a2441b03b
Leif Singer <git@singer.sh>
2014-04-14 22:01:11 +0200

    shorten URLs

--- shorturls/models.py
+++ shorturls/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Link(models.Model):
+	url = models.URLField()
+
+	@staticmethod
+	def shorten(link):
+		l, _ = Link.objects.get_or_create(url=link.url)
+		return str(l.pk)
+
+	@staticmethod
+	def expand(slug):
+		link_id = int(slug)
+		l = Link.objects.get(pk=link_id)
+		return l.url
+
--- shorturls/tests.py
+++ shorturls/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Link
 
-# Create your tests here.
+class ShortenerText(TestCase):
+	def test_shortens(self):
+		"""
+		URLs get shorter
+		"""
+		url = "http://example.com"
+		l = Link(url=url)
+		short_url = Link.shorten(l)
+		self.assertLess(len(short_url), len(url))
+
+	def test_recover_link(self):
+		"""
+		expanded short link is the same as original
+		"""
+		url = "http://example.com"
+		l = Link(url=url)
+		short_url = Link.shorten(l)
+		l.save()
+		exp_url = Link.expand(short_url)
+		self.assertEqual(url, exp_url)
